8f7735b47fbf098fa28f734fcdcfffbea60221a4
Feia Matthijssen <vyrainh@gmail.com>
2021-06-16 23:56:54 +0200

    Early layout and style

---
+++ src/components/Main.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container>
+    <v-layout row>
+
+      <v-col>
+        <v-layout row>
+          <v-col
+              v-for="n in 10"
+              :key="n"
+              cols="auto">
+            <v-card
+                :elevation="5"
+                width="200">
+              <v-img contain :src="require('@/assets/pot.svg')"/>
+              <v-card-text>
+                <v-btn block color="primary">Water</v-btn>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-layout>
+      </v-col>
+
+      <v-col cols="auto">
+        <v-card :elevation="5" height="100%">
+          <v-card-text>
+            <v-card :elevation="0" width="200">
+              <v-img contain :src="require('@/assets/pot.svg')"/>
+              <v-card-text>
+                <v-btn block color="primary">Buy</v-btn>
+              </v-card-text>
+            </v-card>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+
+  data: () => ({}),
+}
+</script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,20 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                background: colors.lightGreen.lighten5,
+                header: colors.lightGreen.lighten1,
+                primary: colors.green,
+            },
+            dark: {
+
+            },
+        },
+    },
 });
